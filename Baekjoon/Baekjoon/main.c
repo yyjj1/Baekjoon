3eb0a42d5ed65485f26f19b0d989acea70f86477
yyjj1 <vkcndfbtkfkd@naver.com>
2019-05-21 21:13:53 +0900

    first init

---
+++ Baekjoon/Baekjoon/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Baekjoon
+//
+//  Created by 이용준 on 21/05/2019.
+//  Copyright © 2019 이용준. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
